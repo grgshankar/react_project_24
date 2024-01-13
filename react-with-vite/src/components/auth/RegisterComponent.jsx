3b298ad2b478f732fce82a847acb2fb55fd8d248
shankar@crisp.digital <shankar@crisp.digital>
2024-01-14 00:20:03 +0545

    adding axios package to work on api to send the data to server

--- react-with-vite/src/components/auth/RegisterComponent.jsx
+++ react-with-vite/src/components/auth/RegisterComponent.jsx
@@ -4,27 +4,37 @@ import { useForm } from "react-hook-form";
 import { NavLink } from "react-router-dom";
 import DefaultImg from "../../assets/images/default_img.png";
 import Select from "react-select";
+import * as Yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 
-const RegisterComponent = () => {
+const RegisterComponent = ({registerSubmit}) => {
+    
     const [thumb, setThumb] = useState();
-    const {register, handleSubmit, setValue, setError, formState: {errors}} = useForm();
     const options = [
         { value: 'seller', label: 'Seller' },
         { value: 'customer', label: 'Buyer' }
       ]
-      const registerSubmit = (data) =>{
-        data.role = data.role.value;
-        console.log(data);
-        
-      }
+    const yupScheme = Yup.object({
+        name: Yup.string().min(2).max(30).required(),
+        email: Yup.string().email().required(),
+        role: Yup.object({
+            value: Yup.string().matches(/seller|customer/),
+            label: Yup.string().matches(/Seller|Buyer/),
+        }).required(),
+    });
+    const {register, handleSubmit, setValue, setError, formState: {errors}} = useForm();
+    
+      
   return (
     <>
       <Form onSubmit={handleSubmit(registerSubmit)} className="register_form text-center mt-4 mb-4">
         <h1>Register</h1>
         <div className="custom_form mt-3">
-          <input type="text" placeholder="Full Name" name="fullname" />
+          <input type="text" placeholder="Full Name" name="fullname" {...register("fullname", {required: true})} />
+          {errors.fullname? (<><em>Name is required</em></>): ""}
           
-          <input type="email" placeholder="Email Address" name="email" />
+          <input type="email" placeholder="Email Address" name="email" {...register("email", {required: true})}/>
+          {errors.email ? (<><em>Email is required</em></>): ""}
           {/* <FormSelect>
             <option value="">--Select any one--</option>
             <option value="seller">Seller</option>
@@ -50,8 +60,10 @@ const RegisterComponent = () => {
             <span className="thum_img_wrapper">
                 <img src={thumb ? URL.createObjectURL(thumb) : DefaultImg} alt="thum_img" />
             </span>
+            {/* {errors?.image?.message} */}
           </div>
-          <input type="password" placeholder="Password" name="password" />
+          <input type="password" placeholder="Password" name="password" {...register("password", {required: true})}/>
+          {errors.password ? (<><em>Password is required</em></>) : ""}
           <div className="btn_wrap">
             <input type="submit" value="Register" className="btn btn-success" />
             <div className="redirect_link">
--- react-with-vite/src/pages/auth/RegisterPage.jsx
+++ react-with-vite/src/pages/auth/RegisterPage.jsx
@@ -1,7 +1,22 @@
 import { NavLink } from "react-router-dom"
 import RegisterComponent from "../../components/auth/RegisterComponent";
-
+import axios from "axios";
 const RegisterPage = () => {
+    const registerSubmit = async (data) =>{
+        
+        try{
+            data.role = data.role.value;
+            console.log(data);
+            const response = await axios.post("https://a4b8c667-bbe6-4271-b6e9-331764d25441-00-1chv3l0oebxil.worf.replit.dev/api/v1", data, {
+                headers : {
+                    "Content-Type" : "multipart/form-data"
+                }
+            });
+            console.log(response);
+        }catch(err){
+            console.log(err)
+        }
+    }
   return (
     <>
     
@@ -9,7 +24,7 @@ const RegisterPage = () => {
         <div className="container">
             <div className="row">
                 <div className="col col-sm-12">
-                    <RegisterComponent />
+                    <RegisterComponent registerSubmit={registerSubmit}/>
                 </div>
             </div>
         </div>
